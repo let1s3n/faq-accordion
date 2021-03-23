6190b4287a3f9006777553db2d40de1d571009d6
let1s3n <letisendev@gmail.com>
2021-03-22 19:14:24 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode:'development',
+  entry: './src/index.js',
+  output: {
+    filename:'main.js',
+    path:path.resolve(__dirname,'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })],
+  output: {
+    clean: true
+  }
+};
