32ef4563f3edad954b7bac70cad2b503b38a2ba8
let1s3n <letisendev@gmail.com>
2021-03-24 15:46:48 -0500

    some minor fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-/* import 'bootstrap/dist/css/bootstrap.min.css' */
-
 import 'bootstrap/js/dist/collapse.js'
-/* import './img/icon-arrow-down.svg'
-import './img/bg-pattern-desktop.svg'
-import './img/bg-pattern-mobile.svg'
-import './img/illustration-box-desktop.svg'
-import './img/illustration-woman-online-desktop.svg'
-import './img/illustration-woman-online-mobile.svg' */
 import './css/styles.scss'
-/* import './img/favicon-32x32.png' */
