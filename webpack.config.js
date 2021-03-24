a43df02f67ef6860dc26abc85357a4c10f8be5de
let1s3n <letisendev@gmail.com>
2021-03-23 20:33:34 -0500

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,51 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  mode:'development',
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename:'main.js',
-    path:path.resolve(__dirname,'dist'),
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/main.js',
+    clean: true
   },
   module: {
     rules: [
       {
         test: /\.((c|sa|sc)ss)$/i,
         use: [
-          "style-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+
+            }
+          },
           "css-loader",
           "sass-loader"
         ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+        use: {
+          loader: "file-loader",
+          options: {
+            name: 'img/[name].[ext]'
+          }
+        }
+      },
+
     ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'src/index.html'
-  })],
-  output: {
-    clean: true
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'css/styles.css'
+    })
+  ]
 };
